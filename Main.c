2d48859d85df9d2678fc5bb5f5f708bb0076601a
Bogdan <tischenkob@gmail.com>
2020-12-02 14:11:37 +0300

    WORKS

--- Main.c
+++ Main.c
@@ -19,17 +19,33 @@ const int OUTPUT_FILE_SIZE = 190 * 1024 * 1024;
 const int IO_BLOCK_SIZE = 114;
 const int NUM_READFILE_THREADS = 107;
 const char *NAME_OUTPUT_FILE = "Output.txt";
-
+const char *NAME_MAX_FILE = "Output_Max.txt";
 volatile int max_int = INT_MIN;
+
+// Synchronisation variables
+pthread_mutex_t MUTEX_OUTPUT_FILE;
+pthread_mutex_t MUTEX_MAX_FILE;
+pthread_mutex_t MUTEX_DEV_URANDOM;
+pthread_cond_t CV_OUTPUT_FILE;
+pthread_cond_t CV_MAX_FILE;
+pthread_cond_t CV_DEV_URANDOM;
 
 // Function declarations
 void *thread_fill_task();
+void *thread_fill_file_task();
 void *fill_memory();
 int *perform_mmap(void *from, size_t size);
 void *thread_count_max_task();
 
 int main()
 {
+    pthread_mutex_init(&MUTEX_OUTPUT_FILE, NULL);
+    pthread_mutex_init(&MUTEX_MAX_FILE, NULL);
+    pthread_mutex_init(&MUTEX_DEV_URANDOM, NULL);
+    pthread_cond_init(&CV_OUTPUT_FILE, NULL);
+    pthread_cond_init(&CV_MAX_FILE, NULL);
+    pthread_cond_init(&CV_DEV_URANDOM, NULL);
+
     printf("-> Before allocation\n");
 
     DEV_URANDOM = fopen("/dev/urandom", "r");
@@ -53,6 +69,9 @@ int main()
 
     printf("-> Let's continue filling the file with garbage!\n");
 
+    pthread_t garbage_refresher_thread;
+    pthread_create(&garbage_refresher_thread, NULL, thread_fill_file_task, NULL);
+
     printf("-> Now let's find max!\n");
 
     pthread_t readfile_threads[NUM_READFILE_THREADS];
@@ -63,6 +82,7 @@ int main()
             printf("Thread failed with return code %d\n", return_code);
     }
 
+    pthread_join(garbage_refresher_thread, NULL);
     for (int i = 0; i < NUM_MMAP_THREADS; i++)
     {
         pthread_join(fillmemory_threads[i], NULL);
@@ -88,7 +108,11 @@ int main()
 
 void *fill_memory()
 {
+    pthread_mutex_lock(&MUTEX_DEV_URANDOM);
+    pthread_cond_wait(&CV_DEV_URANDOM, &MUTEX_DEV_URANDOM);
     fread(MMAP_ADDRESS, MMAP_SIZE, 1, DEV_URANDOM);
+    pthread_cond_broadcast(&CV_DEV_URANDOM);
+    pthread_mutex_unlock(&MUTEX_DEV_URANDOM);
     return NULL;
 }
 
@@ -104,23 +128,36 @@ void *thread_fill_task()
 
 void *thread_count_max_task()
 {
+    printf("-> Thread count! <-\n");
     while (true)
     {
+        pthread_mutex_lock(&MUTEX_OUTPUT_FILE);
+        pthread_cond_wait(&CV_OUTPUT_FILE, &MUTEX_OUTPUT_FILE);
         FILE *file = fopen(NAME_OUTPUT_FILE, "r");
-        /*
+        fseek(file, 0, 0);
         int number = INT_MIN;
         while (!feof(file))
         {
-            fread(&number, sizeof(int), 1, OUTPUT_FILE);
+            printf("-> Prepare to read file! <-\n");
+
+            fread(&number, sizeof(int), 1, file);
+            fflush(file);
+            printf("-> Read %d! <-\n", number);
+            printf("-> Release locks! <-\n");
             if (number > max_int)
             {
-                FILE *max_file = fopen("current_max.txt", "w");
-                fprintf(max_file, "Current max is %d", number);
+                printf("-> Found new max! <-\n");
+                pthread_mutex_lock(&MUTEX_MAX_FILE);
+                FILE *max_file = fopen(NAME_MAX_FILE, "a");
+                printf("Current max is %d\n", number);
+                fprintf(max_file, "Current max is %d\n", number);
                 fclose(max_file);
                 max_int = number;
+                pthread_cond_broadcast(&CV_MAX_FILE);
+                pthread_mutex_unlock(&MUTEX_MAX_FILE);
             }
         }
-        */
+        pthread_mutex_unlock(&MUTEX_OUTPUT_FILE);
         fclose(file);
     }
     return NULL;
@@ -130,7 +167,8 @@ void *thread_fill_file_task()
 {
     while (true)
     {
-        OUTPUT_FILE = fopen(NAME_OUTPUT_FILE, "a");
+        pthread_mutex_lock(&MUTEX_OUTPUT_FILE);
+        FILE *file = fopen(NAME_OUTPUT_FILE, "a");
         srandom(time(NULL)); // Seed randomizer
         int num_io = OUTPUT_FILE_SIZE / IO_BLOCK_SIZE;
         int size_offset = MMAP_SIZE / IO_BLOCK_SIZE;
@@ -139,10 +177,13 @@ void *thread_fill_file_task()
             int address_offset = random() % size_offset;
             void *address = (void *)((int *)MMAP_ADDRESS + address_offset);
             int file_offset = random() & num_io;
-            fseek(OUTPUT_FILE, file_offset, 0);
-            fwrite(address, IO_BLOCK_SIZE, 1, OUTPUT_FILE);
+
+            fseek(file, file_offset, 0);
+            fwrite(address, IO_BLOCK_SIZE, 1, file);
         }
-        fclose(OUTPUT_FILE);
+        pthread_cond_broadcast(&CV_OUTPUT_FILE);
+        pthread_mutex_unlock(&MUTEX_OUTPUT_FILE);
+        fclose(file);
     }
     return NULL;
 }
